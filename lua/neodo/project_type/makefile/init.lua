edb0aeb6119e599d78809532ce323bce69b7ef0b
Jan Valiska <janko@valiska.sk>
2022-08-20 21:55:10 +0200

    Add basic make invokation commands

--- lua/neodo/project_type/makefile/init.lua
+++ lua/neodo/project_type/makefile/init.lua
@@ -1,6 +1,22 @@
+local log = require('neodo.log')
 local M = {}
 
 local settings = require 'neodo.settings'
+
+M.make_cmd = function(ctx)
+    local args = ctx.params
+    local cmd = "make"
+    if type(args) == "table" then
+        for _, arg in ipairs(args) do
+            cmd = cmd .. " " .. arg
+        end
+    elseif type(args) == "string" then
+        cmd = cmd .. " " .. args
+    else
+        log.warning("Unknown params type")
+    end
+    return cmd
+end
 
 M.register = function()
     settings.project_types.makefile = {
@@ -9,6 +25,15 @@ M.register = function()
         on_attach = function()
             -- TODO: parse makefile and create commands
         end,
+        commands = {
+            make = {
+                cmd = M.make_cmd
+            },
+            make_all = {
+                cmd = M.make_cmd,
+                params = "all"
+            }
+        }
     }
 end
 return M
