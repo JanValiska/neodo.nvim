6b61c6fd0840f59f136eb24685be9d140bc5d322
Jan Valiska <janko@valiska.sk>
2023-02-28 23:39:27 +0100

    Get rid of file.lua completely

--- lua/neodo/configuration.lua
+++ lua/neodo/configuration.lua
@@ -3,55 +3,47 @@ local config_file_name = 'config.lua'
 local neodo_folder = '.neodo'
 local dp = vim.fn.stdpath('data')
 local base_data_path = dp .. '/neodo'
-local fs = require('neodo.file')
 local settings = require('neodo.settings')
+local Path = require('plenary.path')
 
-function M.project_hash(project_path)
-    return vim.fn.sha256(project_path)
-end
+function M.project_hash(project_path) return vim.fn.sha256(project_path) end
 
 local function make_percent_path(path)
-    if string.sub(path, 1, 1) == fs.separator then
-        path = path:sub(2)
-    end
-    local p = path:gsub(fs.separator, '-')
+    local paaa = Path:new(path)
+    local sep = paaa._sep
+    if string.sub(path, 1, 1) == sep then path = path:sub(2) end
+    local p = path:gsub(sep, '-')
     return p
 end
 
 local function get_out_of_source_project_data_path(project_path)
-    return fs.join_path(base_data_path, make_percent_path(project_path))
+    return Path:new(base_data_path, make_percent_path(project_path))
 end
 
 local function get_in_the_source_project_data_path(project_path)
-    return fs.join_path(project_path, neodo_folder)
+    return Path:new(project_path, neodo_folder)
 end
 
 function M.get_project_config_and_datapath(project_path)
     local in_source_dp = get_in_the_source_project_data_path(project_path)
-    local in_source_config = fs.join_path(in_source_dp, config_file_name)
-    if fs.file_exists(in_source_config) then
-        return in_source_config, in_source_dp
+    local in_source_config = Path:new(in_source_dp, config_file_name)
+    if in_source_config:exists() then
+        return in_source_config:absolute(), in_source_dp:absolute()
     end
 
     local out_source_dp = get_out_of_source_project_data_path(project_path)
-    local out_source_config = fs.join_path(out_source_dp, config_file_name)
-    if fs.file_exists(out_source_config) then
-        return out_source_config, out_source_dp
+    local out_source_config = Path:new(out_source_dp, config_file_name)
+    if out_source_config:exists() then
+        return out_source_config:absolute(), out_source_dp:absolute()
     end
 
-    if fs.dir_exists(in_source_dp) then
-        return nil, in_source_dp
-    end
+    if in_source_dp:exists() and in_source_dp:is_dir() then return nil, in_source_dp:absolute() end
 
-    if fs.dir_exists(out_source_dp) then
-        return nil, out_source_dp
+    if out_source_dp:exists() and out_source_dp:is_dir() then
+        return nil, out_source_dp:absolute()
     end
 
     return nil, nil
-end
-
-local function write_template(path, template, callback)
-    fs.write(path, 444, template, callback)
 end
 
 local template = [[
@@ -62,18 +54,15 @@ return M
 ]]
 
 local function create_config_file(data_path, callback)
-    if not fs.dir_exists(data_path) then
-        fs.create_directories(data_path)
+    if not data_path:exists() then data_path:mkdir({ parents = true }) end
+    local config_file = Path:new(data_path, config_file_name)
+    config_file:write(template, 'w')
+    if not config_file:exists() then
+        print('Cannot create config file: ' .. config_file)
+        callback(nil, nil)
+        return
     end
-    local config_file = fs.join_path(data_path, config_file_name)
-    write_template(config_file, template, function(err)
-        if err then
-            print('Cannot create config file: ' .. config_file)
-            callback(nil, nil)
-        else
-            callback(config_file, data_path)
-        end
-    end)
+    callback(config_file, data_path)
 end
 
 local function create_out_of_source_config_file(project_path, callback)
@@ -88,13 +77,9 @@ end
 
 function M.ensure_config_file_and_data_path(project, callback)
     local f = create_out_of_source_config_file
-    if settings.use_in_the_source_config then
-        f = create_in_the_source_config_file
-    end
+    if settings.use_in_the_source_config then f = create_in_the_source_config_file end
 
-    f(project:get_path(), function(config, data_path)
-        callback(config, data_path)
-    end)
+    f(project:get_path(), callback)
 end
 
 return M
--- lua/neodo/project_type/cmake/code_model.lua
+++ lua/neodo/project_type/cmake/code_model.lua
@@ -1,6 +1,6 @@
-local fs = require('neodo.file')
 local notify = require('neodo.notify')
 local Path = require('plenary.path')
+local scandir = require('plenary.scandir')
 
 local base_api_dir = Path:new('.cmake', 'api', 'v1')
 local base_query_dir = Path:new(base_api_dir, 'query')
@@ -10,43 +10,40 @@ local base_codemodel_file = 'codemodel-v2'
 local CodeModel = {}
 
 function CodeModel:write_query()
-    if not self.query_dir:is_dir() then
-        self.query_dir:mkdir({ parents = true })
-    end
+    if not self.query_dir:is_dir() then self.query_dir:mkdir({ parents = true }) end
     local codemodel_file = Path:new(self.query_dir, base_codemodel_file)
-    if not codemodel_file:exists() then
-        codemodel_file:touch()
-    end
+    if not codemodel_file:exists() then codemodel_file:touch() end
 end
 
 function CodeModel:load_model_file(path, callback)
-    local full_path = Path:new(self.reply_dir, path)
-    fs.read(full_path:absolute(), 438, function(err, data)
-        if err then
-            notify.error('Cannot read code model file: ' .. path)
-            return
-        else
-            local model = vim.fn.json_decode(data)
-            callback(model)
-        end
-    end)
+    local full_path = Path:new(path)
+    if not full_path:exists() then notify.error('Model file missing:', full_path:absolute()) end
+    local data = full_path:read()
+    if not data then
+        notify.error('Cannot read: ', full_path:absolute())
+        return
+    end
+    local model = vim.fn.json_decode(data)
+    callback(model)
 end
 
 local function find_index_file(reply_dir)
-    local items = fs.dirlist(reply_dir.filename)
-    local pattern = '^index%-.*%.json$'
-    for _, item in ipairs(items) do
-        if string.match(item, pattern) then
-            return item
-        end
+    local pattern = '.*index%-.*%.json$'
+    local function filter(item) return string.match(item, pattern) end
+
+    local items = scandir.scan_dir(reply_dir:absolute(), {
+        search_pattern = filter,
+        hidden = true,
+    })
+    if items then
+        assert(vim.tbl_count(items) == 1, 'None or multiple index files found')
+        return items[1]
     end
     return nil
 end
 
 function CodeModel:parse_target_model(model)
-    if model == nil or model.artifacts == nil then
-        return
-    end
+    if model == nil or model.artifacts == nil then return end
     local paths = {}
     for _, a in ipairs(model.artifacts) do
         table.insert(paths, Path:new(self.build_dir, a.path))
@@ -56,34 +53,37 @@ end
 
 function CodeModel:read_reply(callback)
     local index = find_index_file(self.reply_dir)
-    if index ~= nil then
-        self:load_model_file(index, function(index_model)
-            self:load_model_file(index_model.reply['codemodel-v2'].jsonFile, function(code_model)
+    if index == nil then
+        if type(callback) == 'function' then callback(false) end
+        return
+    end
+
+    self:load_model_file(index, function(index_model)
+        self:load_model_file(
+            Path:new(self.reply_dir, index_model.reply['codemodel-v2'].jsonFile):absolute(),
+            function(code_model)
                 local refs = 0
                 for _, configuration in ipairs(code_model.configurations) do
                     for _, target in ipairs(configuration.targets) do
                         refs = refs + 1
-                        self:load_model_file(target.jsonFile, function(target_model)
-                            self:parse_target_model(target_model)
-                            refs = refs - 1
-                            if refs == 0 and type(callback) == 'function' then
-                                callback(true)
+                        self:load_model_file(
+                            Path:new(self.reply_dir, target.jsonFile):absolute(),
+                            function(target_model)
+                                self:parse_target_model(target_model)
+                                refs = refs - 1
+                                if refs == 0 and type(callback) == 'function' then
+                                    callback(true)
+                                end
                             end
-                        end)
+                        )
                     end
                 end
-            end)
-        end)
-    else
-        if type(callback) == 'function' then
-            callback(false)
-        end
-    end
+            end
+        )
+    end)
 end
 
-function CodeModel:get_targets()
-    return self.targets or {}
-end
+function CodeModel:get_targets() return self.targets or {} end
 
 function CodeModel:new(build_dir)
     local o = {
